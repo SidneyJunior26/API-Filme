73aeb0b7373e128faf574680ffde8fb952e8e90e
Sidney <sidneyjunior26@gmail.com>
2020-08-03 08:29:58 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const port = 3000
+
+app.get('/api/movies/:title', (req, res) => {
+
+    axios.get('https://jsonmock.hackerrank.com/api/movies/search/?Title=' + req.params.title).then(function(resposta){
+
+    var obj = {
+        moviesByYear: [],
+        total: resposta.data.total
+    }
+    
+    for (i in resposta.data.data){
+
+        obj.moviesByYear.push( {
+            year: resposta.data.data[i].Year    
+        })
+    } 
+
+    var json = JSON.stringify(obj)
+
+    res.status(200).send(json)
+
+    }).catch((err) => {
+        if (err){
+            console.log(err)
+        }
+    })
+})
+
+app.get('/api/movies', (req, res) => {
+
+    axios.get('https://jsonmock.hackerrank.com/api/movies').then(function(resposta){
+
+    var obj = {
+        moviesByYear: [],
+        total: resposta.data.total
+    }
+    
+    for (i in resposta.data.data){
+
+        obj.moviesByYear.push( {
+            year: resposta.data.data[i].Year  
+        })
+    }
+
+    var json = JSON.stringify(obj)
+
+    res.status(200).send(json)
+
+    }).catch((err) => {
+        if (err){
+            console.log(err)
+        }
+    })
+})
+
+
+
+app.listen(3000, (req, res) => {
+    console.log('Servidor rodando na porta ' + port)
+})
